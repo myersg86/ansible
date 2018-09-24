88ee3c892f1d42e60af4c9b448e198349bf4fe63
myersg86 <myersg86@gmail.com>
2018-09-24 10:47:27 -0600

    / ‘roles/common/tasks/snap/install-snapd-centos.sh’ / ‘roles/common/tasks/snap/install-snapd.yml’

--- roles/common/tasks/snap/install-snapd-centos.sh
+++ roles/common/tasks/snap/install-snapd-centos.sh
@@ -1,9 +1,9 @@
-sudo yum install epel-release
-sudo yum install yum-plugin-copr
+yum install epel-release
+yum install yum-plugin-copr
 
 # Then add the repo:
 
-sudo yum copr enable ngompa/snapcore-el7
+yum copr enable ngompa/snapcore-el7
 
 #####
 # Loaded plugins: copr, fastestmirror
@@ -14,14 +14,14 @@ sudo yum copr enable ngompa/snapcore-el7
 
 # Once the repository has been added, install snapd package.
 
-sudo yum -y install snapd
+yum -y install snapd
 
 # Wait for the installation to finish then enable snapd socket:
 
-sudo systemctl enable --now snapd.socket
+systemctl enable --now snapd.socket
 
 # Created symlink from /etc/systemd/system/sockets.target.wants/snapd.socket to /usr/lib/systemd/system/snapd.socket.
 
 # Classic confinement requires snaps under /snap or symlink from /snap to /var/lib/snapd/snap. Create a symlink for it like below:
 
-sudo ln -s /var/lib/snapd/snap /snap
+ln -s /var/lib/snapd/snap /snap
