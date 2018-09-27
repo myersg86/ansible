d02723b72b53576df33998cb7c420e7c4988630d
myersg86 <myersg86@gmail.com>
2018-09-27 11:31:37 -0600

    / ‘prepwork-centos.sh’

--- prepwork-centos.sh
+++ prepwork-centos.sh
@@ -2,7 +2,7 @@
 set -v
 
 # Install git & dependencies/recommended (debian/ubuntu)
-yum update &&
+yum update -y &&
 yum install -y git vim python epel-release &&
-yum update &&
-yum install ansible
+yum update -y &&
+yum install -y ansible
